f6831048eec10e9d2885a20530786e8cb24855b7
Erikka Polk <erikka.pokl@gmail.com>
2022-12-14 04:42:11 -0600

    added create functions and routes

--- crud.py
+++ crud.py
@@ -1,49 +1,70 @@
-"This file manipulates the db, saves to it, and reads/queries it via execution functions"
+"This file manipulates the data in the db, saves to it, and reads/queries it via execution functions"
 # (Step 1: the C, U, and D in CRUD functionality) 
 # Manipulating an object/instance with sqlalchemy via python3 
+
+#creating new data, 
+# retrieving data that already exists, 
+# updating data, 
+# and deleting data.
 
 from model import Patron, CollectionFave, ArtFave, RelatedSoundFave, MuseumFave, Collection, RelatedSound, CollectionSound, ArtObject, Museum, connect_to_db
 
 #create all tables in db 
 
-
-
-
-
-
-
-
-
 # (Step2: the R in CRUD) 
 # querying is getting us multiple objects/instances and whatever data using 
 # sqlalchemy via python3
 
-def create_user(name, email, password):
-    pass
+def create_user(email, pword):
+    user=Patron(email, pword)
+    #uname='e', fname='ee', lname='p',
+    return user
 
-def login(email, pword):
-# as a patron i want to login
-    pass
+# def login(email, pword):
+# # as a patron i want to login
+#     pass
 
-def view_collection(id):
+def view_collection(coll_category,name,description, curator):
 # as a patron i want to view a Collection (art, r sounds) 
 # and possibly what museum its at
-    pass
-def view_art(id):
+    collection = Collection(coll_category,name,description, curator)
+    return collection
+
+def view_art(artist, title, medium, description, collection):
+# QUERY
 # as any of these people i want to view art (in collections), filterable, toggable
-    pass
+    art=ArtObject(artist, title, medium, description, collection) #add c3, c2
+    return art
 
-def view_related_sounds(id):
+def view_related_sounds(medium, sound_name, description, museum):
+## QUERY
 # as an art observer i want to see a list of r sounds and hear them
-    pass
+    sound=RelatedSound(medium, sound_name, description, museum) #add m3, m2
+    return sound
 
-def view_museums(id):
+def view_museums(name, city, state, country):
+# QUERY
 # as an art observer i want to view a list of museums and select them and view more
-    pass
+    museum=Museum(name, city, state, country) #removed 'collection=c1'
+    return museum
 
-def view_museums(id):
+# def view_museums(id):
+# QUERY
 # as a patron i want to favorite any 4: collections, art, r sound, Museum
-    pass
+    #  pass
+
+
+
 #need help
 #real-world movie data to populate the movies table before 
 #randomly generating fake users and ratings.
+
+
+
+
+##crud.py deals with db connections, this dunder main will connect you to the database when you run crud.py interactively
+
+if __name__ == '__main__':
+    from server import app
+    with app.app_context():
+        connect_to_db(app)
--- server.py
+++ server.py
@@ -1,18 +1,126 @@
 "This is the server my web app runs on"
 
 from flask import Flask
+from flask import (Flask, render_template, request, flash, session, redirect, jsonify)
+import crud
+from jinja2 import StrictUndefined
 
 app = Flask(__name__)
 
 
-# This is where you'd write your view functions and routes, but there's
-# no need to run server.py during the SQLAlchemy 1 lecture, so
-# we didn't create any routes or view functions here.
+# Route design
+# route name = entity+ Id, + (if possible) action
+
+# I have( 10) tables and about (9) possible pages.
+# view functions: queries and creation
+
+#Homepage:
+@app.route('/')
+def index():
+	"""Displays content of the week and maybe embedded news or the like app page directory"""
+	thing=request.forms.get('key')
+	that=request.args.get('key')
+	return redirect('homepage.html', var=thing)
+
+
+#Login:
+@app.route('/login', methods = ['GET', 'POST'])
+def index():
+	"""Patron Log-in"""
+	thing=request.forms.get('key')
+	that=request.args.get('key')
+	return render_template('login.html', var=thing)
+	#return redirect('homepage.html', var=thing)  #<——redirect to homepage if no login
+
+
+
+
+
+#User Profile:
+@app.route('/profile', methods = ['GET', 'POST'])
+def index():
+	"""Favorites (art, audio guide tours, related sounds, museums)"""
+	thing=request.forms.get('key')
+	that=request.args.get('key')
+	return redirect('user_profile.html', var=thing) #<——redirect to homepage!
+
+
+
+
+
+#Resources:
+@app.route('/resources', methods = ['GET', 'POST'])
+def func_name():
+	"""Proposal for audio guide standardization + Research Links"""
+	thing=request.forms.get('key')
+	that=request.args.get('key')
+	return render_template('resources.html', var=thing) #<——possible redirect to mockup
+
+
+
+
+
+#Mockup:
+@app.route('/near-future-study', methods = ['GET', 'POST'])
+def index():
+	"""Mockup of Related Sounds Repo"""
+	thing=request.forms.get('key')
+	that=request.args.get('key')
+	return render_template('mockup.html', var=thing)
+
+
+
+
+
+#Museum List:
+@app.route('/museumdirectory', methods = ['GET', 'POST'])
+def index():
+	"""describe"""
+	thing=request.forms.get('key')
+	that=request.args.get('key')
+	return render_template('museums.html', var=thing)
+
+
+
+
+
+#Collections:
+@app.route('/collections', methods = ['GET', 'POST'])
+def index():
+	"""curated collections for site + accompanying related sounds"""
+	thing=request.forms.get('key')
+	that=request.args.get('key')
+	return render_template('collections.html', var=thing)
+
+
+
+
+
+#Art Results:
+@app.route('/display-art', methods = ['GET', 'POST'])
+def index():
+	"""Collection content that patron selected!"""
+	thing=request.forms.get('key')
+	that=request.args.get('key')
+	return render_template('login.html', var=thing)
+
+
+
+
+
+#In-Person Audio Guide Tour:
+@app.route('/audio-guide', methods = ['GET', 'POST'])
+def index():
+	"""MOBILE: Museum-specific audio tours, Museum-specific solo visit playlists"""
+	thing=request.forms.get('key')
+	that=request.args.get('key')
+	return render_template('guide.html', var=thing)
+
 
 
 if __name__ == "__main__":
     from model import connect_to_db
 
-    connect_to_db(app, "muse") ############### ADD DB NAME
+    connect_to_db(app, "muse") 
 
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0") #change when depolying FIX ME
