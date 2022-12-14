f2353a94a6332f3b2001e0e59669d1885ebbbfdf
Erikka Polk <erikka.pokl@gmail.com>
2022-12-14 14:36:07 -0600

    minor adjustment to routes

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-"This is the server my web app runs on"
+# "This is the server my web app runs on"
 
 from flask import Flask
 from flask import (Flask, render_template, request, flash, session, redirect, jsonify)
@@ -8,119 +8,119 @@ from jinja2 import StrictUndefined
 app = Flask(__name__)
 
 
-# Route design
-# route name = entity+ Id, + (if possible) action
+# # Route design
+# # route name = entity+ Id, + (if possible) action
 
-# I have( 10) tables and about (9) possible pages.
-# view functions: queries and creation
+# # I have( 10) tables and about (9) possible pages.
+# # view functions: queries and creation
 
-#Homepage:
-@app.route('/')
-def index():
-	"""Displays content of the week and maybe embedded news or the like app page directory"""
-	thing=request.forms.get('key')
-	that=request.args.get('key')
-	return redirect('homepage.html', var=thing)
-
-
-#Login:
-@app.route('/login', methods = ['GET', 'POST'])
-def index():
-	"""Patron Log-in"""
-	thing=request.forms.get('key')
-	that=request.args.get('key')
-	return render_template('login.html', var=thing)
-	#return redirect('homepage.html', var=thing)  #<——redirect to homepage if no login
+# #Homepage:
+# @app.route('/')
+# def index():
+# 	"""Displays content of the week and maybe embedded news or the like app page directory"""
+# 	thing=request.forms.get('key')
+# 	that=request.args.get('key')
+# 	return redirect('index.html', var=thing)
 
 
-
-
-
-#User Profile:
-@app.route('/profile', methods = ['GET', 'POST'])
-def index():
-	"""Favorites (art, audio guide tours, related sounds, museums)"""
-	thing=request.forms.get('key')
-	that=request.args.get('key')
-	return redirect('user_profile.html', var=thing) #<——redirect to homepage!
+# #Login:
+# @app.route('/login', methods = ['GET', 'POST'])
+# def index():
+# 	"""Patron Log-in"""
+# 	thing=request.forms.get('key')
+# 	that=request.args.get('key')
+# 	return render_template('login.html', var=thing)
+# 	#return redirect('login.html', var=thing)  #<——redirect to homepage if no login
 
 
 
 
 
-#Resources:
-@app.route('/resources', methods = ['GET', 'POST'])
-def func_name():
-	"""Proposal for audio guide standardization + Research Links"""
-	thing=request.forms.get('key')
-	that=request.args.get('key')
-	return render_template('resources.html', var=thing) #<——possible redirect to mockup
+# #User Profile:
+# @app.route('/profile', methods = ['GET', 'POST'])
+# def index():
+# 	"""Favorites (art, audio guide tours, related sounds, museums)"""
+# 	thing=request.forms.get('key')
+# 	that=request.args.get('key')
+# 	return redirect('user-profile.html', var=thing) #<——redirect to homepage!
 
 
 
 
 
-#Mockup:
-@app.route('/near-future-study', methods = ['GET', 'POST'])
-def index():
-	"""Mockup of Related Sounds Repo"""
-	thing=request.forms.get('key')
-	that=request.args.get('key')
-	return render_template('mockup.html', var=thing)
+# #Resources:
+# @app.route('/resources', methods = ['GET', 'POST'])
+# def func_name():
+# 	"""Proposal for audio guide standardization + Research Links"""
+# 	thing=request.forms.get('key')
+# 	that=request.args.get('key')
+# 	return render_template('resources.html', var=thing) #<——possible redirect to mockup
 
 
 
 
 
-#Museum List:
-@app.route('/museumdirectory', methods = ['GET', 'POST'])
-def index():
-	"""describe"""
-	thing=request.forms.get('key')
-	that=request.args.get('key')
-	return render_template('museums.html', var=thing)
+# #Mockup:
+# @app.route('/near-future-study', methods = ['GET', 'POST'])
+# def index():
+# 	"""Mockup of Related Sounds Repo"""
+# 	thing=request.forms.get('key')
+# 	that=request.args.get('key')
+# 	return render_template('mockup.html', var=thing)
 
 
 
 
 
-#Collections:
-@app.route('/collections', methods = ['GET', 'POST'])
-def index():
-	"""curated collections for site + accompanying related sounds"""
-	thing=request.forms.get('key')
-	that=request.args.get('key')
-	return render_template('collections.html', var=thing)
+# #Museum List:
+# @app.route('/museumdirectory', methods = ['GET', 'POST'])
+# def index():
+# 	"""describe"""
+# 	thing=request.forms.get('key')
+# 	that=request.args.get('key')
+# 	return render_template('museums.html', var=thing)
 
 
 
 
 
-#Art Results:
-@app.route('/display-art', methods = ['GET', 'POST'])
-def index():
-	"""Collection content that patron selected!"""
-	thing=request.forms.get('key')
-	that=request.args.get('key')
-	return render_template('login.html', var=thing)
+# #Collections:
+# @app.route('/collections', methods = ['GET', 'POST'])
+# def index():
+# 	"""curated collections for site + accompanying related sounds"""
+# 	thing=request.forms.get('key')
+# 	that=request.args.get('key')
+# 	return render_template('collections.html', var=thing)
 
 
 
 
 
-#In-Person Audio Guide Tour:
-@app.route('/audio-guide', methods = ['GET', 'POST'])
-def index():
-	"""MOBILE: Museum-specific audio tours, Museum-specific solo visit playlists"""
-	thing=request.forms.get('key')
-	that=request.args.get('key')
-	return render_template('guide.html', var=thing)
+# #Individual Collection Page:
+# @app.route('/<int: collection_id>', methods = ['GET', 'POST'])
+# def index():
+# 	"""Collection content that patron selected!"""
+# 	thing=request.forms.get('key')
+# 	that=request.args.get('key')
+# 	return render_template('view-collection.html', var=thing)
 
 
 
-if __name__ == "__main__":
-    from model import connect_to_db
 
-    connect_to_db(app, "muse") 
 
-    app.run(debug=True, host="0.0.0.0") #change when depolying FIX ME
+# #In-Person Audio Guide Tour:
+# @app.route('/audio-guide', methods = ['GET', 'POST'])
+# def index():
+# 	"""MOBILE: Museum-specific audio tours, Museum-specific solo visit playlists"""
+# 	thing=request.forms.get('key')
+# 	that=request.args.get('key')
+# 	return render_template('guide.html', var=thing)
+
+
+
+# if __name__ == "__main__":
+#     from model import connect_to_db
+
+#     connect_to_db(app, "muse") 
+
+#     app.run(debug=True, host="0.0.0.0") #change when depolying FIX ME
