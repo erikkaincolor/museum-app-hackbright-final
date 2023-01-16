8e79aa47f6313bbc66a958faf7a0de580066ea12
Erikka Polk <erikka.pokl@gmail.com>
2023-01-15 19:18:57 -0600

    bs button and nav css overrides, new images,  edited base.html

--- static/js/sound_favorite.js
+++ static/js/sound_favorite.js
@@ -13,11 +13,10 @@
 // output:clicking on button shows us info
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////     SOUNDS in COLLECTION OBJ FAVES
+/////////////////////////     SOUNDS in COLLECTION, MUSEUM OBJ FAVES
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//logged-in/sound + add sound fave
-let btn5 = document.querySelector("#addsoundfave"); //for the add to faves btn
+let btn5 = document.querySelector("#addsoundfave");
 
 const soundBtn = (evt)=>{
     let soundId = evt.target.value;
@@ -31,44 +30,22 @@ const soundBtn = (evt)=>{
         }
     )
 };
+btn5.addEventListener("click", soundBtn)
 
-btn5.addEventListener("click", soundBtn); //i want this to be for first button 
-btn7.addEventListener("click", soundBtn); //and the second to be for the second
 
-//logged-in + remove sound fave
-let btn6 = document.querySelector("#removesoundfave"); //for the add to faves btn
+
+
+
+
+
+
+
+// logged-in + remove sound fave-APPROVED
+let btn6 = document.querySelector("#removesoundfave"); 
 
 btn6.addEventListener("click", (evt)=>{
     const soundId=evt.target.value  
+    // console.log(collectionId)
     fetch(`/${soundId}/removesoundfavorites`, {method:"POST"}) //AJAX request to db
 })
 
-
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////     SOUNDS in MUSEUM OBJ FAVES
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-//logged-in/sound + add sound fave
-let btn7 = document.querySelector("#addsoundfave"); //for the add to faves btn
-
-// btn7.addEventListener("click", (evt)=>{
-//     let soundId = evt.target.value;
-//     fetch(`/${soundId}/soundfavorites`, {method:"POST"})
-//     .then((response)=> response.json())
-//     .then((result)=>{
-//             //conditional;if result
-//             if (result.status === "FAIL"){
-//                 alert("You must log in to favorite a sound.")
-//             }
-//         }
-//     )
-// })
-
-//logged-in + remove sound fave
-let btn8 = document.querySelector("#removesoundfave"); //for the add to faves btn
-
-btn8.addEventListener("click", (evt)=>{
-    const soundId=evt.target.value  
-    fetch(`/${soundId}/removesoundfavorites`, {method:"POST"}) //AJAX request to db
-})
